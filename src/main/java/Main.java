d476285f022e73b8e69d8a995570d48691357ddd
philipmabee <philipmabee1234@gmail.com>
2026-01-09 15:53:08 -0500

    for loop stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,9 @@ package main.java;
 public class Main {
     public static void main(String[] args) {
         Logic logicPuzzle = new Logic();
-
         System.out.println(logicPuzzle.checkCorrect());
+
+		  ForLoop forloop = new ForLoop();
+		  System.out.println(forloop.checkCorrect());
     }
 }
