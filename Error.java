5e210f21ae6bd8fb2bd3f8a9324ec82dd92a1011
jhaase565-arch <jhaase565@gmail.com>
2026-02-06 17:37:09 -0500

    I fixed all the errors in this code

--- Error.java
+++ Error.java
@@ -1,12 +1,35 @@
 package main.java;
 
 public class Error {{
-    int j = "Hello";
-    char number = "one";
-    String place = 'notastring'
+    int j = 4;
+    char number = 1;
+    String place = "notastring";
     int string = 1;
-    for (int i == 0, j > 10, i++){
-        System.out.println "";
+    for (int i = 0; j > 10; i++){
+        System.out.println();
+    }
+    switch (string){
+        case 1:
+            place = "Wendy's";
+            break;
+        case 2:
+            j = 12;
+            break;
+        case 3:
+            number = 8;
+            break; 
+
+    }
+}
+}package main.java;
+
+public class Error {{
+    int j = 4;
+    char number = 1;
+    String place = "notastring";
+    int string = 1;
+    for (int i = 0; j > 10; i++){
+        System.out.println();
     }
     switch (string){
         case 1:
